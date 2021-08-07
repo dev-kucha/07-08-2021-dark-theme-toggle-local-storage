794069c43ed0928405372f3c41c9207b9ea45d75
Yurii <ukucher@gmail.com>
2021-08-07 20:24:31 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Reference: 
+WebDev с нуля. Канал Алекса Лущенко
+<a href="https://youtu.be/1-ryNAqehm8">День - ночь на сайте. Переключатель тем на JavaScript</a>
+
---
+++ script.js
@@ -0,0 +1,27 @@
+document.querySelector('.themetoggle').addEventListener('click', (event) => {
+    event.preventDefault();
+    console.log('click');
+
+    if (localStorage.getItem('theme') === 'dark') {
+        localStorage.removeItem('theme');
+    }
+    else {
+        localStorage.setItem('theme', 'dark')
+    }
+    addDarkClassToHTML();
+});
+
+function addDarkClassToHTML() {
+    try {
+        if (localStorage.getItem('theme') === 'dark') {
+            document.querySelector('html').classList.add('dark');
+            document.querySelector('.themetoggle span').textContent = 'dark_mode';
+        }
+        else {
+            document.querySelector('html').classList.remove('dark');
+            document.querySelector('.themetoggle span').textContent = 'wb_sunny';
+        }
+    } catch (err) { }
+}
+
+addDarkClassToHTML();
